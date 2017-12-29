d500054dbcaf98497179ec1c6878174db8aa00d7
WouldChar <wouldchar@gmail.com>
2017-12-29 14:16:39 +0800

    Backport udp2raw-tunnel and UDPspeeder

--- files/luci/model/cbi/shadowsocksr/client.lua
+++ files/luci/model/cbi/shadowsocksr/client.lua
@@ -1,7 +1,7 @@
 -- Copyright (C) 2017 yushi studio <ywb94@qq.com> github.com/ywb94
 -- Licensed to the public under the GNU General Public License v3.
 
-local m, s,sec, o,kcp_enable
+local m, s, sec, o, kcp_enable
 local shadowsocksr = "shadowsocksr"
 local uci = luci.model.uci.cursor()
 local ipkg = require("luci.model.ipkg")
@@ -71,7 +71,37 @@ obfs = {
 	"tls1.2_ticket_auth",
 }
 
+local raw_mode = {
+	"faketcp",
+	"udp",
+	"icmp",
+}
 
+local seq_mode = {
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+}
+
+local cipher_mode = {
+	"none",
+	"xor",
+	"aes128cbc",
+}
+
+local auth_mode = {
+	"none",
+	"simple",
+	"md5",
+	"crc32",
+}
+
+local speeder_mode = {
+	"0",
+	"1",
+}
 
 uci:foreach(shadowsocksr, "servers", function(s)
 	if s.alias then
@@ -80,8 +110,6 @@ uci:foreach(shadowsocksr, "servers", function(s)
 		server_table[s[".name"]] = "%s:%s" %{s.server, s.server_port}
 	end
 end)
-
-
 
 -- [[ Servers Setting ]]--
 sec = m:section(TypedSection, "servers", translate("Servers Setting"))
@@ -218,6 +246,105 @@ o.datatype = "port"
 o.default = 1234
 o.rmempty = false
 
+-- [[ udp2raw ]]--
+if nixio.fs.access("/usr/bin/udp2raw") then
+
+s = m:section(TypedSection, "udp2raw", translate("udp2raw tunnel"))
+s.anonymous = true
+
+o = s:option(Flag, "udp2raw_enable", translate("Enable udp2raw"))
+o.default = 0
+o.rmempty = false
+
+o = s:option(Value, "server", translate("Server Address"))
+o.datatype = "host"
+o.rmempty = false
+
+o = s:option(Value, "server_port", translate("Server Port"))
+o.datatype = "port"
+o.rmempty = false
+
+o = s:option(Value, "local_port", translate("Local Port"))
+o.datatype = "port"
+o.rmempty = false
+
+o = s:option(Value, "key", translate("Password"))
+o.password = true
+o.rmempty = false
+
+o = s:option(ListValue, "raw_mode", translate("Raw Mode"))
+for _, v in ipairs(raw_mode) do o:value(v) end
+o.default = "faketcp"
+o.rmempty = false
+
+o = s:option(ListValue, "seq_mode", translate("Seq Mode"))
+for _, v in ipairs(seq_mode) do o:value(v) end
+o.default = "3"
+o.rmempty = false
+
+o = s:option(ListValue, "cipher_mode", translate("Cipher Mode"))
+for _, v in ipairs(cipher_mode) do o:value(v) end
+o.default = "xor"
+o.rmempty = false
+
+o = s:option(ListValue, "auth_mode", translate("Auth Mode"))
+for _, v in ipairs(auth_mode) do o:value(v) end
+o.default = "simple"
+o.rmempty = false
+
+end
+
+-- [[ udpspeeder ]]--
+if nixio.fs.access("/usr/bin/udpspeeder") then
+
+s = m:section(TypedSection, "udpspeeder", translate("UDPspeeder"))
+s.anonymous = true
+
+o = s:option(Flag, "udpspeeder_enable", translate("Enable UDPspeeder"))
+o.default = 0
+o.rmempty = false
+
+o = s:option(Value, "server", translate("Server Address"))
+o.datatype = "host"
+o.rmempty = false
+
+o = s:option(Value, "server_port", translate("Server Port"))
+o.datatype = "port"
+o.rmempty = false
+
+o = s:option(Value, "local_port", translate("Local Port"))
+o.datatype = "port"
+o.rmempty = false
+
+o = s:option(Value, "key", translate("Password"))
+o.password = true
+o.rmempty = false
+
+o = s:option(ListValue, "speeder_mode", translate("Speeder Mode"))
+for _, v in ipairs(speeder_mode) do o:value(v) end
+o.default = "0"
+o.rmempty = false
+
+o = s:option(Value, "fec", translate("Fec"))
+o.default = "20:10"
+o.rmempty = false
+
+o = s:option(Value, "mtu", translate("Mtu"))
+o.datatype = "uinteger"
+o.default = 1250
+o.rmempty = false
+
+o = s:option(Value, "queue_len", translate("Queue Len"))
+o.datatype = "uinteger"
+o.default = 200
+o.rmempty = false
+
+o = s:option(Value, "timeout", translate("Fec Timeout"))
+o.datatype = "uinteger"
+o.default = 8
+o.rmempty = false
+
+end
 
 -- [[ Access Control ]]--
 s = m:section(TypedSection, "access_control", translate("Access Control"))
