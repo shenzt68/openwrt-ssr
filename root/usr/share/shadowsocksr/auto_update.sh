fe8359c887fda2d30fb16a5824461133a01c4faa
Ivan <ivanhe1986@gmail.com>
2019-08-30 17:00:49 -0400

    Auto update fixes

--- root/usr/share/shadowsocksr/auto_update.sh
+++ root/usr/share/shadowsocksr/auto_update.sh
@@ -15,6 +15,8 @@ echo_date(){
 
 Server_Update() {
     local uci_set="uci -q set $name.$1."
+    ${uci_set}grouphashkey="$ssr_grouphashkey"
+    ${uci_set}hashkey="$ssr_hashkey"
     ${uci_set}alias="[$ssr_group] $ssr_remarks"
     ${uci_set}auth_enable="0"
     ${uci_set}switch_enable="1"
@@ -34,32 +36,36 @@ Server_Update() {
     ${uci_set}kcp_port="0"
     ${uci_set}kcp_param="--nocomp"
     
-    #v2ray
-    ${uci_set}alter_id="$ssr_alter_id"
-    ${uci_set}vmess_id="$ssr_vmess_id"
-    ${uci_set}security="$ssr_security"
-    ${uci_set}transport="$ssr_transport"
-    ${uci_set}tcp_guise="$ssr_tcp_guise"
-	${uci_set}tls="$ssr_tls"
+	#v2ray
+	if [ "$ssr_type" = "v2ray" ]; then
+	    ${uci_set}alter_id="$ssr_alter_id"
+	    ${uci_set}vmess_id="$ssr_vmess_id"
+	    ${uci_set}security="$ssr_security"
+	    ${uci_set}transport="$ssr_transport"
+	    ${uci_set}tcp_guise="$ssr_tcp_guise"
+	    ${uci_set}ws_host="$ssr_ws_host"
+	    ${uci_set}ws_path="$ssr_ws_path"
+	    ${uci_set}tls="$ssr_tls"
+	fi
 }
 
 name=shadowsocksr
-
-echo_date "Updating Subscription..."
-
-subscribe_url=($(uci get $name.@server_subscribe[0].subscribe_url))
+subscribe_url=($(uci get $name.@server_subscribe[0].subscribe_url)) #订阅服务器地址
 [ ${#subscribe_url[@]} -eq 0 ] && exit 1
 [ $(uci -q get $name.@server_subscribe[0].proxy || echo 0) -eq 0 ] && /etc/init.d/$name stop >/dev/null 2>&1
 log_name=${name}_subscribe
 for ((o=0;o<${#subscribe_url[@]};o++))
 do
-	echo_date "Updating from ${subscribe_url[o]}"
-	echo_date "Downloading the link，please wait..."
+	echo_date "从 ${subscribe_url[o]} 获取订阅"
+	echo_date "开始更新在线订阅列表..."
+	echo_date "开始下载订阅链接到本地临时文件，请稍等..."
 	subscribe_data=$(wget-ssl --user-agent="User-Agent: Mozilla" --no-check-certificate -T 3 -O- ${subscribe_url[o]})
 	curl_code=$?
+	# 计算group的hashkey
+	ssr_grouphashkey=$(echo "${subscribe_url[o]}" | md5sum | cut -d ' ' -f1)
 	if [ ! $curl_code -eq 0 ];then
-		echo_date "Subscription data downloaded..."
-		echo_date "Resolving node info..."
+		echo_date "下载订阅成功..."
+		echo_date "开始解析节点信息..."
 		subscribe_data=$(wget-ssl --no-check-certificate -T 3 -O- ${subscribe_url[o]})
 		curl_code=$?
 	fi
@@ -84,7 +90,7 @@ do
 			else
 				subscribe_max=${#ssr_url[@]}
 			fi
-			echo_date "$subscribe_max nodes in total"
+			echo_date "共计$subscribe_max个节点"
 			ssr_group=$(urlsafe_b64decode $(urlsafe_b64decode ${ssr_url[$((${#ssr_url[@]} - 1))]//ssr:\/\//} | sed 's/&/\n/g' | grep group= | awk -F = '{print $2}'))
 			if [ -z "$ssr_group" ]; then
 				ssr_group="default"
@@ -98,9 +104,10 @@ do
 					curr_ssr=$(uci show $name | grep @servers | grep -c server=)
 					for ((x=0;x<$curr_ssr;x++)) # 循环已有服务器信息，匹配当前订阅群组
 					do
-						temp_alias=$(uci -q get $name.@servers[$x].alias | grep "\[$ssr_group\]")
-						[ -n "$temp_alias" ] && temp_host_o[${#temp_host_o[@]}]=$(uci get $name.@servers[$x].server)
+						temp_alias=$(uci -q get $name.@servers[$x].grouphashkey | grep "$ssr_grouphashkey")
+						[ -n "$temp_alias" ] && temp_host_o[${#temp_host_o[@]}]=$(uci get $name.@servers[$x].hashkey)
 					done
+
 					for ((x=0;x<$subscribe_max;x++)) # 循环链接
 					do
 						[ ${#subscribe_max_x[@]} -eq 0 ] && temp_x=$x || temp_x=${subscribe_max_x[x]}
@@ -108,6 +115,9 @@ do
 						if [[ "$result" != "" ]]
 						then
 							temp_info=$(urlsafe_b64decode ${ssr_url[temp_x]//ssr:\/\//}) # 解码 SSR 链接
+							# 计算hashkey
+							ssr_hashkey=$(echo "$temp_info" | md5sum | cut -d ' ' -f1)
+
 
 							info=${temp_info///?*/}
 							temp_info_array=(${info//:/ })
@@ -140,6 +150,9 @@ do
 				done
 			else
 				temp_info=$(urlsafe_b64decode ${ssr_url[temp_x]//vmess:\/\//}) # 解码 Vmess 链接
+				# 计算hashkey
+				ssr_hashkey=$(echo "$temp_info" | md5sum | cut -d ' ' -f1)
+
 				ssr_type="v2ray"
 				json_load "$temp_info"
 				json_get_var ssr_host add
@@ -149,26 +162,32 @@ do
 				json_get_var ssr_security type
 				json_get_var ssr_transport net
 				json_get_var ssr_remarks ps
-				json_get_var ssr_tls_tmp tls
-				if [ "$ssr_tls_tmp" == "tls" ] ;then
+				ssr_tcp_guise="none"
+				json_get_var ssr_ws_host host
+				json_get_var ssr_ws_path path
+				json_get_var ssr_tls tls
+				if [ "$ssr_tls" == "tls" -o "$ssr_tls" == "1" ]; then
 					ssr_tls="1"
 				else
-					ssr_tls="0"
+				    ssr_tls="0"
 				fi
-				ssr_tcp_guise="none"
 			fi
 
+			if [ -z "ssr_remarks" ]; then # 没有备注的话则生成一个
+				ssr_remarks="$ssr_host:$ssr_port";
+			fi
 
-			uci_name_tmp=$(uci show $name | grep -w $ssr_host | awk -F . '{print $2}')
+			uci_name_tmp=$(uci show $name | grep -w "$ssr_hashkey" | awk -F . '{print $2}')
 			if [ -z "$uci_name_tmp" ]; then # 判断当前服务器信息是否存在
 				uci_name_tmp=$(uci add $name servers)
 				subscribe_n=$(($subscribe_n + 1))
 			fi
 			Server_Update $uci_name_tmp
-			subscribe_x=$subscribe_x$ssr_host" "
+			subscribe_x=$subscribe_x$ssr_hashkey" "
 			ssrtype=$(echo $ssr_type | tr '[a-z]' '[A-Z]')
-			echo_date "$ssrtype node：【$ssr_remarks】"
-
+			echo_date "$ssrtype节点：【$ssr_remarks】"
+			
+			# SSR
 			# echo "服务器地址: $ssr_host"
 			# echo "服务器端口 $ssr_port"
 			# echo "密码: $ssr_passwd"
@@ -178,6 +197,7 @@ do
 			# echo "混淆: $ssr_obfs"
 			# echo "混淆参数: $ssr_obfsparam"
 			# echo "备注: $ssr_remarks"
+			
 		done
 		for ((x=0;x<${#temp_host_o[@]};x++)) # 新旧服务器信息匹配，如果旧服务器信息不存在于新服务器信息则删除
 		do
@@ -187,18 +207,18 @@ do
 				subscribe_o=$(($subscribe_o + 1))
 			fi
 		done
-		echo_date "Subscription Source:【$ssr_group】 Number of nodes: ${#ssr_url[@]} New nodes: $subscribe_n Deleted nodes: $subscribe_o"
-		echo_date "Subscription update completed!"
-		subscribe_log="$ssr_group Subscription update completed! Number of nodes: ${#ssr_url[@]} New nodes: $subscribe_n Deleted nodes: $subscribe_o"
+		echo_date "本次更新订阅来源 【$ssr_group】 服务器数量: ${#ssr_url[@]} 新增服务器: $subscribe_n 删除服务器: $subscribe_o"
+		echo_date "在线订阅列表更新完成!请等待网页自动刷新!"
+		subscribe_log="$ssr_group 服务器订阅更新成功 服务器数量: ${#ssr_url[@]} 新增服务器: $subscribe_n 删除服务器: $subscribe_o"
 		logger -st $log_name[$$] -p6 "$subscribe_log"
 		uci commit $name
 	else
-		echo_date "${subscribe_url[$o]} Fail to resolve subscription data, unable to get Group"
-		logger -st $log_name[$$] -p3 "${subscribe_url[$o]} Fail to resolve subscription data, unable to get Group"
+		echo_date "${subscribe_url[$o]} 订阅数据解析失败 无法获取 Group"
+		logger -st $log_name[$$] -p3 "${subscribe_url[$o]} 订阅数据解析失败 无法获取 Group"
 	fi
 else
-	echo_date "${subscribe_url[$o]} Fail to get subscription data, error code: $curl_code"
-	logger -st $log_name[$$] -p3 "${subscribe_url[$o]} Fail to get subscription data, error code: $curl_code"
+	echo_date "${subscribe_url[$o]} 订阅数据获取失败 错误代码: $curl_code"
+	logger -st $log_name[$$] -p3 "${subscribe_url[$o]} 订阅数据获取失败 错误代码: $curl_code"
 fi
 done
 
